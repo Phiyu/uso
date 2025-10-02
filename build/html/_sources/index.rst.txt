acb9c1e33c06a98366f63492d439cba5da1b0549
Phiyu <yuhongfei@mail.ustc.edu.cn>
2025-10-02 22:57:32 +0800

    init

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+.. uso-manual documentation master file, created by
+   sphinx-quickstart on Thu Oct  2 22:42:48 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+中国科学技术大学学生管弦乐团社团指北   
+========================
+
+这里是中国科学技术大学学生管弦乐团（USO，简称校学生管弦乐团）社团指北，你可以在这里看到：
+- 社团简介
+- 社团规章
+- 新生必读
+- 团务工作指南
+- 历次演出总结
+- Q&A
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
---
+++ source/conf.py
@@ -0,0 +1,29 @@
+import sphinx_rtd_theme
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'uso-manual'
+copyright = '2025, 中国科学技术大学学生管弦乐团'
+author = '中国科学技术大学学生管弦乐团'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
